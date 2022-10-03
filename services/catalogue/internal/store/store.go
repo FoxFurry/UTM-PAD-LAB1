7067bdb3ff402dbf9fa5c214291bf8a332f98193
FoxFurry <isacartur@gmail.com>
2022-10-03 20:07:04 +0300

    Added datastore interface

--- services/catalogue/internal/store/store.go
+++ services/catalogue/internal/store/store.go
@@ -1,1 +1,18 @@
 package store
+
+import (
+	"database/sql"
+)
+
+type Catalogue interface {
+}
+
+type catalogue struct {
+	db *sql.DB
+}
+
+func NewCatalogueStore(db *sql.DB) Catalogue {
+	return catalogue{
+		db: db,
+	}
+}
