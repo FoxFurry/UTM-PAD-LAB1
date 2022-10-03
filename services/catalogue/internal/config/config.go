fb217b283f69be929ffd94a86b1b128822162bf1
FoxFurry <isacartur@gmail.com>
2022-10-03 10:22:57 +0300

    Added config struct

--- services/catalogue/internal/config/config.go
+++ services/catalogue/internal/config/config.go
@@ -1,1 +1,9 @@
 package config
+
+type Config struct {
+	DBName string `mapstructure:"db_name"`
+	DBHost string `mapstructure:"db_host"`
+	DBUser string `mapstructure:"db_user"`
+	DBPass string `mapstructure:"db_pass"`
+	DBPort string `mapstructure:"db_port"`
+}
