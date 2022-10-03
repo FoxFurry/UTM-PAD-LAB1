9cbf92f349404d6cbbea7655ad16e8414196fb83
FoxFurry <isacartur@gmail.com>
2022-10-03 20:05:55 +0300

    Added store to catalogue server

--- services/catalogue/internal/server/catalogue.go
+++ services/catalogue/internal/server/catalogue.go
@@ -4,21 +4,26 @@ import (
 	"context"
 
 	"google.golang.org/protobuf/types/known/emptypb"
+	"pad/services/catalogue/internal/store"
 	catalogue "pad/services/catalogue/proto"
 )
 
 type Catalogue struct {
+	db store.Catalogue
+
 	catalogue.UnimplementedCatalogueServer
 }
 
-func NewCatalogueServer() catalogue.CatalogueServer {
-	return &Catalogue{}
+func NewCatalogueServer(db store.Catalogue) catalogue.CatalogueServer {
+	return &Catalogue{
+		db: db,
+	}
 }
 
 func (c *Catalogue) AddListing(context.Context, *catalogue.Listing) (*emptypb.Empty, error) {
-
+	return &emptypb.Empty{}, nil
 }
 
 func (c *Catalogue) GetAllListings(context.Context, *emptypb.Empty) (*catalogue.GetListingsResponse, error) {
-
+	return &catalogue.GetListingsResponse{}, nil
 }
