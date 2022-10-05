c6c5bb2cb231472df40d8fa109c4777b7095db0d
FoxFurry <isacartur@gmail.com>
2022-10-06 00:03:08 +0300

    Added GetListingByTitle server function

--- services/catalogue/internal/server/catalogue.go
+++ services/catalogue/internal/server/catalogue.go
@@ -9,21 +9,65 @@ import (
 )
 
 type Catalogue struct {
-	db store.Catalogue
+	datastore store.Catalogue
 
 	catalogue.UnimplementedCatalogueServer
 }
 
 func NewCatalogueServer(db store.Catalogue) catalogue.CatalogueServer {
 	return &Catalogue{
-		db: db,
+		datastore: db,
 	}
 }
 
-func (c *Catalogue) AddListing(context.Context, *catalogue.Listing) (*emptypb.Empty, error) {
+func (c *Catalogue) AddListing(ctx context.Context, l *catalogue.AddListingRequest) (*emptypb.Empty, error) {
+	err := c.datastore.AddListing(ctx, &store.Listing{
+		Title:        l.GetListing().Title,
+		Description:  l.GetListing().Description,
+		ThumbnailURL: l.GetListing().ThumbnailUrl,
+		AuthorID:     69, // TODO(Arthur): Change to actual author id
+	})
+	if err != nil {
+		return nil, err
+	}
+
 	return &emptypb.Empty{}, nil
 }
 
-func (c *Catalogue) GetAllListings(context.Context, *emptypb.Empty) (*catalogue.GetListingsResponse, error) {
-	return &catalogue.GetListingsResponse{}, nil
+func (c *Catalogue) GetAllListings(ctx context.Context, _ *emptypb.Empty) (*catalogue.GetAllListingsResponse, error) {
+	listings, err := c.datastore.GetAllListings(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return &catalogue.GetAllListingsResponse{
+		Listings: listingStoreToProto(listings),
+	}, nil
+}
+
+func (c *Catalogue) GetListingByTitle(ctx context.Context, req *catalogue.GetListingByTitleRequest) (*catalogue.GetListingByTitleResponse, error) {
+	listing, err := c.datastore.GetListingByTitle(ctx, req.GetTitle())
+	if err != nil {
+		return nil, err
+	}
+
+	return &catalogue.GetListingByTitleResponse{
+		Listing: &catalogue.Listing{
+			Title:        listing.Title,
+			Description:  listing.Description,
+			ThumbnailUrl: listing.ThumbnailURL,
+		},
+	}, nil
+}
+
+func listingStoreToProto(l []store.Listing) []*catalogue.Listing {
+	var buffer []*catalogue.Listing
+	for idx := range l {
+		buffer = append(buffer, &catalogue.Listing{
+			Title:        l[idx].Title,
+			Description:  l[idx].Description,
+			ThumbnailUrl: l[idx].ThumbnailURL,
+		})
+	}
+	return buffer
 }
