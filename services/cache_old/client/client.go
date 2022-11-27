9ed04e9d02e1ae533ee1785945da15fc15c8cd36
FoxFurry <isacartur@gmail.com>
2022-11-27 16:51:02 +0200

    Moved previous cache implementation to cache_old

--- services/cache_old/client/client.go
+++ services/cache_old/client/client.go
@@ -3,7 +3,7 @@ package client
 import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
-	"pad/services/cache/services/cache"
+	"pad/services/cache_old/services/cache"
 )
 
 func NewCacheClient(address string) (cache.CacheClient, error) {
